01e8f8c68075a50c5b98b708344974bd2e59ddb2
marcos <jmarcosuarez@gmail.com>
2023-03-27 20:58:54 +0100

    Health check handler and intergration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on out Server
+    run(address)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,38 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    // We need to bring in 'request'
+    // to perform HTTP requests against out application
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
+        .send().await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+// no .await call, therefore no need for 'spawn_app' to be async now
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// al the things
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future
+    //but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+
+    // We return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
+}
