1f996edeed94ae683c671d58d5b6d8f418b78459
marcos <jmarcosuarez@gmail.com>
2023-03-30 20:34:18 +0100

    Add tracing logs

--- README.md
+++ README.md
@@ -10,15 +10,13 @@ From root run
 ./scripts/init_db.sh
 ```
 
-to get database migration started
-or, if docker ps returns postgres container already, run
-
-```
-SKIP_DOCKER=true ./scripts/init_db.sh
-```
-
 then run
 
 ```
 cargo watch -x check -x test -x fmt -x run
 ```
+
+## Logging
+
+We default to print all logs at info level or above. As if we did run the app with the env variable `RUST_LOG` set to `info`. Eg. `RUST_LOG=info cargo run`.
+Other values can be `trace`, `debug`, `info`, `warn` and `error`.
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::PgPool;
+use sqlx::{query, PgPool};
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -14,26 +15,49 @@ pub async fn subscribe(
     // Retrieving a connection from the application state
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    // Generates a random unique identifier to properly correlate logs
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+            "Adding a new subscriber.",
+    %request_id,
+    subscriber_email = %form.email,
+    subscriber_name = %form.name,
+        );
+    let _request_span_guard = request_span.enter();
+
+    // we do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     // `Result` has 2 variant: `Ok` and `Err`.
     // The first for successes, the second for failures
     // We use a `match` statement to choose what to do based on the outcome
-    match sqlx::query(
+    match query!(
         r#"
                     INSERT INTO subscriptions (id, email, name, subscribed_at) 
                     VALUES ($1, $2, $3, $4)
                 "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
     )
-    .bind(Uuid::new_v4())
-    .bind(&form.email)
-    .bind(&form.name)
-    .bind(Utc::now())
     // Using the pool as a replacement for PgConnection
     .execute(pool.as_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            println!("Failed to excecute query: {}", e);
+            // yes, this error log falls outside of the `query_span`
+            // we will fix it later
+            tracing::error!(
+                "Request id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
