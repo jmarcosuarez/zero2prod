f1b325b2c2be36487904d968f57f794ba2c945ca
marcos <jmarcosuarez@gmail.com>
2023-04-08 10:39:39 +0100

    Configure email client with reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -15,7 +16,12 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
-
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
     // We are reading address from Settings
     let address = format!(
         "{}:{}",
@@ -25,5 +31,5 @@ async fn main() -> std::io::Result<()> {
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on out Server
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
