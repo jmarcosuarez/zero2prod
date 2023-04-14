bf14accdcdf5e9de1be84a0238a1238e957a49fc
marcos <jmarcosuarez@gmail.com>
2023-04-14 16:48:50 +0100

    Add subscriptions token also add transactions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,10 +1,11 @@
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
-
 use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::{query, PgPool};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use sqlx::{query, PgPool, Postgres, Transaction};
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
@@ -52,6 +53,12 @@ pub async fn subscribe(
     email_client: web::Data<EmailClient>,
     base_url: web::Data<ApplicationBaseUrl>,
 ) -> HttpResponse {
+    // transactions got its own API
+    // to begin on our pool we acquire a connection from the pool and kick off a transaction
+    let mut transaction = match pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
     // We implemented `TryFrom` but we are calling `.try_into()`
     // `TryFrom implementation  gives you this for free
     // ` form.0.try_into()` equals `NewSubscriber::try_from(from.0)`
@@ -61,14 +68,31 @@ pub async fn subscribe(
         // Return early if the email is invalid, with a 400
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
+        Ok(subscriber_id) => subscriber_id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    let subscription_token = generate_subscription_token();
+    if store_token(&mut transaction, subscriber_id, &subscription_token)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+    // Explicitly commit all changes to the Transaction before it oges out of scope.
+    // Otherwise all changes would be rolled back
+    if transaction.commit().await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
 
-    if send_confirmation_email(&email_client, new_subscriber, &base_url.0)
-        .await
-        .is_err()
+    if send_confirmation_email(
+        &email_client,
+        new_subscriber,
+        &base_url.0,
+        &subscription_token,
+    )
+    .await
+    .is_err()
     {
         return HttpResponse::InternalServerError().finish();
     }
@@ -77,16 +101,17 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Send a confirmation email to new subscriber",
-    skip(email_client, new_subscriber, base_url)
+    skip(email_client, new_subscriber, base_url, subscription_token)
 )]
 pub async fn send_confirmation_email(
     email_client: &EmailClient,
     new_subscriber: NewSubscriber,
     base_url: &str,
+    subscription_token: &str,
 ) -> Result<(), reqwest::Error> {
     let confirmation_link = format!(
-        "{}/subscriptions/confirm?subscription_token=mytoken",
-        base_url
+        "{}/subscriptions/confirm?subscription_token={}",
+        base_url, subscription_token,
     );
     // Subscriptions will send an email
     let html_body = &format!(
@@ -106,26 +131,27 @@ pub async fn send_confirmation_email(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+    skip(new_subscriber, transaction)
 )]
 
 pub async fn insert_subscriber(
-    pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     // Retrieving a connection from the application state
     new_subscriber: &NewSubscriber,
-) -> Result<(), sqlx::Error> {
+) -> Result<Uuid, sqlx::Error> {
+    let subscriber_id = Uuid::new_v4();
     query!(
         r#"
                     INSERT INTO subscriptions (id, email, name, subscribed_at, status) 
                     VALUES ($1, $2, $3, $4, 'pending_confirmation')
                 "#,
-        Uuid::new_v4(),
+        subscriber_id,
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
-    // Using the pool as a replacement for PgConnection
-    .execute(pool)
+    // Use the passed transaction instead of pool
+    .execute(transaction)
     .await
     .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
@@ -134,7 +160,7 @@ pub async fn insert_subscriber(
         // if the function failed, returning a sqlx::Error
     })?;
 
-    Ok(())
+    Ok(subscriber_id)
 }
 
 /// Returns `true` if the input satisfies all our validation constrains
@@ -160,4 +186,37 @@ pub fn is_valid_name(s: &str) -> bool {
 
     // Return `false` if any of our conditions have been violated
     !(is_empty_or_whitespace || is_too_long || contain_forbidden_characters)
+}
+
+// Generate a random 25-characters-log case-sensitive subscription token.
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
+}
+
+#[tracing::instrument(
+    name = "Store subscription token in the database",
+    skip(subscription_token, transaction)
+)]
+pub async fn store_token(
+    transaction: &mut Transaction<'_, Postgres>,
+    subscriber_id: Uuid,
+    subscription_token: &str,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"INSERT INTO subscription_tokens (subscription_token, subscriber_id)
+        VALUES ($1, $2)"#,
+        subscription_token,
+        subscriber_id,
+    )
+    .execute(transaction)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query {:?}", e);
+        e
+    })?;
+    Ok(())
 }
